822682eea45a2d279bdf9fc1f85d23a465cadc8d
dengwenj <927561237@qq.com>
2022-04-24 01:31:36 +0800

    头部布局

---
+++ src/components/Header/index.vue
@@ -0,0 +1,134 @@
+<template>
+  <header class='app-header'>
+    <div class="container">
+      <h1 class="logo">
+        <router-link to="/">
+          <img class="img" src="../../assets/images/dwjww.png" alt="">
+          <div class="name">购物商城</div>
+        </router-link>
+      </h1>
+      <ul class="navs">
+        <li class="home"><router-link to="/">首页</router-link></li>
+        <li><a href="#">美食</a></li>
+        <li><a href="#">餐厨</a></li>
+        <li><a href="#">艺术</a></li>
+        <li><a href="#">电器</a></li>
+        <li><a href="#">居家</a></li>
+        <li><a href="#">洗护</a></li>
+        <li><a href="#">孕婴</a></li>
+        <li><a href="#">服装</a></li>
+        <li><a href="#">杂货</a></li>
+      </ul>
+      <div class="search">
+        <i class="iconfont icon-search"></i>
+        <input type="text" placeholder="搜一搜">
+      </div>
+      <div class="cart">
+        <a class="curr" href="#">
+          <i class="iconfont icon-cart"></i><em>2</em>
+        </a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped lang='less'>
+.app-header {
+  background: #fff;
+  .container {
+    display: flex;
+    align-items: center;
+  }
+  .logo {
+    width: 200px;
+    a {
+      position: relative;
+      display: block;
+      height: 132px;
+      width: 100%;
+      .img {
+        position: absolute;
+        top: 50%;
+        left: 45%;
+        transform: translate(-50%, -50%);
+        border-radius: 5px;
+        width: 50px;
+        height: 50px;
+      }
+      .name {
+        position: absolute;
+        top: 41%;
+        left: 62%;
+        font-size: 18px;
+        color: @themeColor;
+      }
+    }
+    
+  }
+  .navs {
+    width: 820px;
+    display: flex;
+    justify-content: space-around;
+    padding-left: 40px;
+    li {
+      margin-right: 40px;
+      width: 38px;
+      text-align: center;
+      a {
+        font-size: 16px;
+        line-height: 32px;
+        height: 32px;
+        display: inline-block;
+      }
+      &:hover {
+        a {
+          color: @themeColor;
+          border-bottom: 1px solid @themeColor;
+        }
+      }
+    }
+  }
+  .search {
+    width: 170px;
+    height: 32px;
+    position: relative;
+    border-bottom: 1px solid #e7e7e7;
+    line-height: 32px;
+    .icon-search {
+      font-size: 18px;
+      margin-left: 5px;
+    }
+    input {
+      width: 140px;
+      padding-left: 5px;
+      color: #666;
+    }
+  }
+  .cart {
+    width: 50px;
+    .curr {
+      height: 32px;
+      line-height: 32px;
+      text-align: center;
+      position: relative;
+      display: block;
+      .icon-cart{
+        font-size: 22px;
+      }
+      em {
+        font-style: normal;
+        position: absolute;
+        right: 0;
+        top: 0;
+        padding: 1px 6px;
+        line-height: 1;
+        // background: @helpColor;
+        color: #fff;
+        font-size: 12px;
+        border-radius: 10px;
+        font-family: Arial;
+      }
+    }
+  }
+}
+</style>
--- src/pages/layout/index.vue
+++ src/pages/layout/index.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import Navbar from '@/components/NavBar/index.vue'
+import Header from '@/components/Header/index.vue'
 </script>
 
 <template>
   <div class="container">
     <!-- 顶部通栏 -->
     <Navbar />
-
+    <!-- 头部 -->
+    <Header />
     <main>
       <!-- 二级路由 -->
       <router-view />
