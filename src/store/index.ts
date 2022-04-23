e56cf19f2f8996a4d7f81573b2b6d349808f49c9
dengwenj <927561237@qq.com>
2022-04-23 18:11:25 +0800

    vuex and vue-router

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,11 @@
-import { createStore } from 'vuex'
+import { createStore, Store, useStore as useVuexStore } from 'vuex'
 
 const store = createStore({
-  
+  state: {
+    count: 0
+  }
 })
+
+export const useStore = <T = any>(): Store<T> => useVuexStore()
 
 export default store
