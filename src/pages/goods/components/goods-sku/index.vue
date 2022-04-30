e73e44be2c9c5ff82c511f3847c73231fbfd103f
dengwenj <927561237@qq.com>
2022-04-30 23:38:48 +0800

    设置状态

--- src/pages/goods/components/goods-sku/index.vue
+++ src/pages/goods/components/goods-sku/index.vue
@@ -1,11 +1,72 @@
 <script setup lang="ts">
 import { defineProps } from 'vue'
 
-defineProps<{
+import getPowerSet from '@/global/power-set.js'
+
+const props = defineProps<{
   goods: any
 }>()
 
+const spliter = '★'
+// 根据skus数据得到路径字典对象
+const getPathMap = (skus: any[]) => {
+  const pathMap = {}
+  skus.forEach(sku => {
+    // 1. 过滤出有库存有效的sku
+    if (sku.inventory) {
+      // 2. 得到sku属性值数组
+      const specs = sku.specs.map((spec: any) => spec.valueName)
+      // 3. 得到sku属性值数组的子集
+      const powerSet = getPowerSet(specs)
+      // 4. 设置给路径字典对象
+      powerSet.forEach((set: any) => {
+        const key = set.join(spliter)
+        if ((pathMap as any)[key]) {
+          // 已经有key往数组追加
+          (pathMap as any)[key].push(sku.id)
+        } else {
+          // 没有key设置一个数组
+          (pathMap as any)[key] = [sku.id]
+        }
+      })
+    }
+  })
+  return pathMap
+}
+const pathMap = getPathMap(props.goods.skus)
+// console.log(pathMap)
+
+// 得到当前选中规格集合
+const getSelectedArr = (specs: any[]) => {
+  const selectedArr: any[] = []
+  specs.forEach((spec: any) => {
+    const selectedVal = spec.values.find((val: any) => val.selected)
+    selectedArr.push(selectedVal ? selectedVal.name : undefined)
+  })
+  return selectedArr
+}
+
+// 更新按钮的禁用状态
+const updateDisabledStatus = (specs: any[], pathMap: Record<string, any>) => {
+  specs.forEach((spec, i) => {
+    const selectedArr = getSelectedArr(specs)
+    spec.values.forEach((val: any) => {
+      // 已经选中的按钮不用判断
+      if (val.name === selectedArr[i]) return false
+      // 未选中的替换对应的值
+      selectedArr[i] = val.name
+      // 过滤无效值得到key
+      const key = selectedArr.filter(v => v).join(spliter)
+      // 设置禁用状态
+      val.disabled = !pathMap[key]
+    })
+  })
+}
+updateDisabledStatus(props.goods.specs, pathMap)
+
 const handleClick = (values: any[], val: Record<string, any>) => {
+  if (val.disabled) return false
+
   if (val.selected) {
     val.selected = false
   } else {
@@ -14,6 +75,8 @@ const handleClick = (values: any[], val: Record<string, any>) => {
     })
     val.selected = true
   }
+
+  updateDisabledStatus(props.goods.specs, pathMap)
 }
 </script>
 
@@ -23,8 +86,19 @@ const handleClick = (values: any[], val: Record<string, any>) => {
       <dt>{{ item.name }}</dt>
       <dd>
         <template v-for="val in item.values" :key="val.name">
-          <img :class="val.selected ? 'selected' : ''" @click="handleClick(item.values, val)" v-if="val.picture" :src="val.picture" :title="val.name">
-          <span :class="val.selected ? 'selected' : ''" @click="handleClick(item.values, val)" v-else>{{ val.name }}</span>
+          <img
+            :class="{ selected: val.selected, disabled: val.disabled }" 
+            @click="handleClick(item.values, val)" 
+            v-if="val.picture" 
+            :src="val.picture" :title="val.name"
+          >
+          <span
+            v-else
+            :class="{ selected: val.selected, disabled: val.disabled }" 
+            @click="handleClick(item.values, val)" 
+          >
+            {{ val.name }}
+          </span>
         </template>
       </dd>
     </dl>
