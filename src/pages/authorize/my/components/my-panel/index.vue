23c9a352a026bc85bfad51547d9dd09b954dd0d5
dengwenj <927561237@qq.com>
2022-05-07 22:05:07 +0800

    基础布局

---
+++ src/pages/authorize/my/components/my-panel/index.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+import WwMore from '@/components/lib/WwMore.vue'
+
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <div class="home-panel">
+    <div class="header">
+      <h4>{{ title }}</h4>
+      <WwMore to="/" />
+    </div>
+    <!-- 商品列表 -->
+    <div class="goods-list">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="less">
+.home-panel {
+  background-color: #fff;
+  padding: 0 20px;
+  margin-top: 20px;
+  height: 400px;
+  .header {
+    height: 66px;
+    border-bottom: 1px solid #f5f5f5;
+    padding: 18px 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
+    h4 {
+      font-size: 22px;
+      font-weight: normal;
+    }
+  }
+}
+</style>
--- src/pages/authorize/my/index.vue
+++ src/pages/authorize/my/index.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts">
-
+import MyOverview from './components/my-overview/index.vue'
+import MyPanel from './components/my-panel/index.vue'
+import GoodsRelevant from '@/pages/goods/components/goods-relevant/index.vue'
 </script>
 
 <template>
-  <div>my</div>
+  <div class="member-home">
+    <!-- 概览 -->
+    <MyOverview />
+    <!-- 收藏 -->
+    <MyPanel title="我的收藏"></MyPanel>
+    <!-- 足迹 -->
+    <MyPanel title="我的足迹"></MyPanel>
+    <!-- 猜你 -->
+    <GoodsRelevant />
+  </div>
 </template>
+
+<style scoped lang="less">
+:deep(.xtx-carousel) .carousel-btn.prev {
+  left: 5px;
+}
+:deep(.xtx-carousel) .carousel-btn.next {
+  right: 5px;
+}
+</style>
+
