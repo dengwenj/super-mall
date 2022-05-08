eacd242a257a7f179bca14aa50ddd6795e33e0c0
dengwenj <927561237@qq.com>
2022-05-08 19:02:45 +0800

    激活选项卡

--- src/components/WwTabs/index.tsx
+++ src/components/WwTabs/index.tsx
@@ -4,47 +4,74 @@ import './index.less'
 
 import type { SetupContext } from 'vue'
 
+interface IWwTabsProps { 
+  active: string
+  updateActive: (name: string) => void
+}
+
 // function WwTabs 这个相当于 setup 函数
-export default defineComponent(function WwTabs(props, ctx: SetupContext) {
-  const dynamicNode: any[] = []
+export default defineComponent<IWwTabsProps>(function WwTabs(props, ctx: SetupContext) {
+  // 点击每一项
+  const handleClick = (name: string) => {
+    return () => {
+      (ctx.attrs.updateActive as (name: string) => void)(name)
+    }
+  }
 
-  return () => (
-    <div class="xtx-tabs">
-      {/* tabs */}
-      <nav>
-        {
-          ctx.slots.default?.().forEach((item, idx) => {
-            if ((item.type as any).name === 'WwTabsPanel') {
-              dynamicNode.push(item)
-            } else {
-              (item.children as any).forEach((item2: any) => {
-                dynamicNode.push(item2)
-              })
-            }
-          })
-        }
-        {
-          dynamicNode.map((item) => {
-            return (
-              <a href="javascript:;" key={item.key!}>
-                {item.props?.label}
-              </a>
-            )
-          })
-        }
-      </nav>
+  return () => {
+    const dynamicNode: any[] = []
 
-      {/* 内容 */}
-      {ctx.slots.default?.()}
-      {/* <div>
+    return (
+      <div class="xtx-tabs">
+        {/* tabs */}
+        <nav>
+          {
+            ctx.slots.default?.().forEach((item, idx) => {
+              if ((item.type as any).name === 'WwTabsPanel') {
+                dynamicNode.push(item)
+              } else {
+                (item.children as any).forEach((item2: any) => {
+                  dynamicNode.push(item2)
+                })
+              }
+            })
+          }
+          {
+            dynamicNode.map((item) => {
+              return (
+                <a
+                  class={ctx.attrs.active === item.props.name ? 'active' : ''}
+                  onClick={handleClick(item.props.name)} 
+                  href="javascript:;" 
+                  key={item.key!}
+                >
+                  {item.props?.label}
+                </a>
+              )
+            })
+          }
+        </nav>
+
+        {/* 内容 */}
         {
-          ctx.slots.default?.().map((item, idx) => {
-            return (
-              <div key={item.key!}>{(item.children as any).default()}</div>
-            )
-          })
+          (function () {
+            let obj: any
+            const panel = ctx.slots.default?.().find((item) => {
+              if ((item.type as any).name === 'WwTabsPanel') {
+                return item.props?.name === ctx.attrs.active
+              } else {
+                for (let i = 0; i < (item.children as any).length; i++) {
+                  if (ctx.attrs.active === (item.children as any)[i].props.name) {
+                    obj = (item.children as any)[i]
+                    return
+                  }
+                }
+              }
+            })
+            return obj || panel
+          })()
         }
-      </div> */}
-    </div>
-  )
+      </div>
+    )
+  }
 })
--- src/pages/authorize/order/index.tsx
+++ src/pages/authorize/order/index.tsx
@@ -1,21 +1,31 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 import WwTabs from '@/components/WwTabs'
 import WwTabsPanel from '@/components/WwTabs/panel'
 
 export default defineComponent(function Order() {
+  const active = ref('name')
+
+  const updateActive = (name: string) => {
+    active.value = name
+  }
+
   return () => (
-    <>
-      <WwTabs>
-        <WwTabsPanel label={`选项卡`} name={`name`}>内容</WwTabsPanel>
-        {
-          [1, 2, 3 , 4].map((item) => {
-            return (
-              <WwTabsPanel label={`选项卡${item}`} name={`name${item}`} key={item}>内容{item}</WwTabsPanel>
-            )
-          })
-        }
-      </WwTabs>
-    </>
+    <WwTabs active={active.value} updateActive={updateActive}>
+      <WwTabsPanel label={`选项卡`} name={`name`}>内容</WwTabsPanel>
+      {
+        [1, 2, 3 , 4].map((item) => {
+          return (
+            <WwTabsPanel 
+              label={`选项卡${item}`} 
+              name={`name${item}`} 
+              key={item}
+            >
+              内容{item}
+            </WwTabsPanel>
+          )
+        })
+      }
+    </WwTabs>
   )
 })
