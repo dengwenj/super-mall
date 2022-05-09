2b0533e9ac0a1f34761138a583f1ef101c14e52b
dengwenj <927561237@qq.com>
2022-05-09 22:12:22 +0800

    步骤条

--- src/pages/authorize/order/detail.vue
+++ src/pages/authorize/order/detail.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watchEffect } from 'vue'
 import { useRoute } from 'vue-router'
+import { ElSteps, ElStep } from 'element-plus'
 
 import DetailAction from './components/detail-action/index.vue'
 
 import { getOrderDetail } from '@/services/api/order'
 
 const order = ref()
+const active = ref(0)
+
+watchEffect(() => {
+  active.value = order?.value?.orderState
+})
 
 const route = useRoute()
 
@@ -21,6 +27,33 @@ onMounted(async () => {
     <!-- 操作栏 -->
     <DetailAction :order="order" />
     <!-- 步骤条 -->
+    <ElSteps style="padding: 30px 0px" process-status="wait" align-center :active="active" finish-status="success">
+      <ElStep title="提交订单">
+        <template #description>
+          {{ order.createTime }}
+        </template>
+      </ElStep>
+      <ElStep title="付款成功">
+        <template #description>
+          {{ order.payTime }}
+        </template>
+      </ElStep>
+      <ElStep title="商品发货">
+        <template #description>
+          {{ order.consignTime }}
+        </template>
+      </ElStep>
+      <ElStep title="确定收货">
+        <template #description>
+          {{ order.evaluationTime }}
+        </template>
+      </ElStep>
+      <ElStep title="订单完成">
+        <template #description>
+          {{ order.endTime }}
+        </template>
+      </ElStep>
+    </ElSteps>
     <!-- 物流栏 -->
     <!-- 订单商品信息 -->
   </div>
@@ -30,5 +63,15 @@ onMounted(async () => {
 .order-detail-page {
   background: #fff;
   height: 100%;
+}
+/deep/.el-step__head.is-success {
+  color: @themeColor;
+  border-color: @themeColor;
+}
+/deep/.el-step__title.is-success {
+  color: @themeColor;
+}
+/deep/.el-step__description.is-success {
+  color: @themeColor;
 }
 </style>
